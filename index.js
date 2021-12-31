24f51d3763e841b0c767a8c58ee57c1cda2a5e60
Eirik Vågeskar <playground.gate@protonmail.com>
2021-12-31 08:35:51 +0100

    :recycle: Make a pure trapese calculation file

--- index.js
+++
@@ -1,7 +0,0 @@
-export function leggSammen(a, b) {
-  return a + b
-}
-
-export function multipliser(a, b) {
-  return a * b
-}
---
+++ trapes.js
@@ -0,0 +1,7 @@
+function leggSammen(a, b) {
+  return a + b
+}
+
+export function regnUtArealAvTrapes(topplengde, bunnlengde, hoeyde) {
+  return leggSammen(topplengde, bunnlengde) * hoeyde / 2
+}
